20bee1d4b98dbe68765f4921bee3affcdfec9d84
Yin-Chen Kris Huang <krishuang168@Yin-Chende-MacBook.local>
2020-07-20 09:09:45 -0700

    Data

---
+++ public/data/restaurants/BellevueWA.js
@@ -0,0 +1,32 @@
+const BellevueWA = [
+  {
+    name: "Kizuki Ramen & Izakaya (Main St)",
+    category: "Ramen",
+    streetAddress: "14845 Main St",
+    city: "Bellevue",
+    state: "WA",
+    zip: "98007",
+    phone: "+14257777048",
+    tempStoreHours: "11:00AM - 9:30PM",
+    maps: "https://goo.gl/maps/D5zBR1L2PmY8a1qe9",
+    DineIn: true,
+    uberEats: true,
+    toGo: true,
+  },
+  {
+    name: "Wingstop",
+    category: "Chicken Wings",
+    streetAddress: "14875 Main St",
+    city: "Bellevue",
+    state: "WA",
+    zip: "98007",
+    phone: "+14259741142",
+    tempStoreHours: "11:00AM - 11:00PM",
+    maps: "https://goo.gl/maps/cZNFExC3wobtkbrA7",
+    DineIn: false,
+    uberEats: true,
+    toGo: true,
+  },
+];
+
+exports.BellevueWA;
