176baebc3214bfcaa653026f904cac953441ec73
Yin-Chen Kris Huang <krishuang168@Yin-Chende-MacBook.local>
2020-07-20 04:52:21 -0700

    Homepage & CSS

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const fs = require("fs");
+const router = express.Router();
+
+const frontend = `${__dirname}/../../../html-website/`;
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  // res.render('index', { title: 'Express' });
+  const homePage = fs.readFileSync(
+    "../../html-website/public/index.html",
+    "utf8"
+  );
+  console.log(frontend);
+  res.send(homePage);
 });
+
+/* Load CSS */
+router.use("/css", express.static("../../html-website/public/css"));
 
 module.exports = router;
