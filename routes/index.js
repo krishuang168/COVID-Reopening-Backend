1bdd696b91175f729d9be8c3a7b717862c1a1a41
Yin-Chen Kris Huang <job.krishuang@gmail.com>
2020-07-22 08:19:42 -0700

    Change to const & locationsRouter

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,19 @@ const router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  const page = fs.readFileSync(frontendPath + "index.html", "utf8");
-  res.send(page);
+  try {
+    const page = fs.readFileSync(frontendPath + "index.html", "utf8");
+    res.statusCode = 200;
+    res.send(page);
+  } catch (err) {
+    res.statusCode = 404;
+    res.end(`
+    <h1 style="color: #995500;
+    text-shadow: 2px 2px #c99f4a;
+    font-size: 3.2em;
+    font-family: "Tangerine", sans-serif;">Coming Soon</h1>
+    `);
+  }
 });
 
 /* Load CSS, JS & node_modules */
@@ -17,16 +28,23 @@ router.use("/css", express.static(frontendPath + "css"));
 router.use("/js", express.static(frontendPath + "js"));
 router.use("/node_modules", express.static(frontendPath + "node_modules"));
 
-/* By Location */
-router.get("/locations", function (req, res, next) {
-  const page = fs.readFileSync(frontendPath + "locations.html", "utf8");
-  res.send(page);
-});
+
 
 /* By Category */
 router.get("/categories", function (req, res, next) {
-  const page = fs.readFileSync(frontendPath + "categories.html", "utf8");
-  res.send(page);
+  try {
+    const page = fs.readFileSync(frontendPath + "categories.html", "utf8");
+    res.statusCode = 200;
+    res.send(page);
+  } catch (err) {
+    res.statusCode = 404;
+    res.end(`
+    <h1 style="color: #995500;
+    text-shadow: 2px 2px #c99f4a;
+    font-size: 3.2em;
+    font-family: "Tangerine", sans-serif;">Coming Soon</h1>
+    `);
+  }
 });
 
 /* Photo Credits */
