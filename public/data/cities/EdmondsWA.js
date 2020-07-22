b353d2ef8ed9091a396de05757d09ef5c0366837
Yin-Chen Kris Huang <job.krishuang@gmail.com>
2020-07-22 06:47:32 -0700

    Folder Rename

---
+++ public/data/cities/EdmondsWA.js
@@ -0,0 +1,32 @@
+const EdmondsWA = [
+  {
+    name: "BlackBall Taiwanese Dessert",
+    category: "Taiwanese",
+    streetAddress: "22001 Hwy 99 Foyer",
+    city: "Edmonds",
+    state: "WA",
+    zip: "98026",
+    phone: "+14256739711",
+    tempStoreHours: "11:00AM - 11:00PM",
+    maps: "https://goo.gl/maps/LVFpTQtwRfNvhtCF6",
+    DineIn: true,
+    uberEats: true,
+    toGo: true,
+  },
+  {
+    name: "Looking for Chai Taiwanese Kitchen",
+    category: "Taiwanese",
+    streetAddress: "22511 Hwy 99 Suite 100",
+    city: "Edmonds",
+    state: "WA",
+    zip: "98026",
+    phone: "+14256720880",
+    tempStoreHours: "11:00AM - 11:00PM",
+    maps: "https://goo.gl/maps/BrUYo8iQ1Kds12ia7",
+    DineIn: false,
+    uberEats: false,
+    toGo: false,
+  },
+];
+
+exports.EdmondsWA;
---
+++ public/data/cities/WoodinvilleWA.js
@@ -0,0 +1,32 @@
+const WoodinvilleWA = [
+  {
+    name: "Vivi Pizzeria",
+    category: "Italian",
+    streetAddress: "14505 148th Ave NE",
+    city: "Woodinville",
+    state: "WA",
+    zip: "98072",
+    phone: "(425)408-0711",
+    tempStoreHours: "11:00AM - 9:00PM",
+    maps: "",
+    DineIn: true,
+    uberEats: true,
+    toGo: true,
+  },
+  {
+    name: "Village Wines",
+    category: "Italian",
+    streetAddress: "14450 Redmond-Woodinville Rd NE",
+    city: "Woodinville",
+    state: "WA",
+    zip: "98072",
+    phone: "(425)485-3536",
+    tempStoreHours: "12:00PM - 10:00PM",
+    maps: "",
+    DineIn: true,
+    uberEats: true,
+    toGo: true,
+  },
+];
+
+exports.WoodinvilleWA;
