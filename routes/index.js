22208a23388db3f785a4ad3879c8c3a111a6ec3f
Yin-Chen Kris Huang <job.krishuang@gmail.com>
2020-07-22 03:24:19 -0700

    By Location & By Category

--- routes/index.js
+++ routes/index.js
@@ -8,13 +8,25 @@ const router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  const homePage = fs.readFileSync(frontendPath + "index.html", "utf8");
-  res.send(homePage);
+  const page = fs.readFileSync(frontendPath + "index.html", "utf8");
+  res.send(page);
 });
 
 /* Load CSS, JS & node_modules */
 router.use("/css", express.static(frontendPath + "css"));
 router.use("/js", express.static(frontendPath + "js"));
 router.use("/node_modules", express.static(frontendPath + "node_modules"));
+
+/* By Location */
+router.get("/locations", function (req, res, next) {
+  const page = fs.readFileSync(frontendPath + "locations.html", "utf8");
+  res.send(page);
+});
+
+/* By Category */
+router.get("/categories", function (req, res, next) {
+  const page = fs.readFileSync(frontendPath + "categories.html", "utf8");
+  res.send(page);
+});
 
 module.exports = router;
